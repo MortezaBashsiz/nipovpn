6d379d1be7cc5e7fa30984324a7cf851799055e0
Morteza Bashsiz <morteza.bashsiz@gmail.com>
2023-07-11 16:10:15 +0200

    add asio

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,6 +1,6 @@
 #include "main.hpp"
 
-int main(int argc=0, char* argv[]={0}){
+int main(int argc=0, char* argv[]=0){
 	mainArgs mainArgs = validateMainArgs(argc, argv);
 	Config nipoConfig(mainArgs.configPath);
 	Log nipoLog(nipoConfig.config.logFile, nipoConfig.config.logLevel);
