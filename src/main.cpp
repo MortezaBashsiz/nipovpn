21f8db22ad6cce0a653c7bb618d8c7bc6f69f840
Morteza Bashsiz <morteza.bashsiz@gmail.com>
2023-07-14 10:46:38 +0200

    add test file

--- src/main.cpp
+++ src/main.cpp
@@ -19,13 +19,3 @@ int main(int argc=0, char* argv[]=0){
 	}
   return 0;
 };
-
-// unsigned char plain[] = "This is text";
-// unsigned char key[] = "iuwahchu5aingohtoutoiveing9ahd4GoiZo4dai9eezaeloov6Kae4taiquooGohk3ERaR8Cheid8tae4Quohgiash7fai8giuShaingee4be0oib6ahbaich1veo2MeeYoh2Ophuoxafie1ge1wei3moo7shainied3eez5oe2uculohs8Cu8OoquaePee1weirie4eCunohb4Hei4eo5Oopukon6quath5mieGee4eivooCohmai5uu7Aex7v";
-// unsigned int plainLen = 16 * sizeof(plain);
-// AES aes(AESKeyLength::AES_256);
-// unsigned char *out = aes.EncryptECB(plain, plainLen, key);
-//  unsigned char *innew = aes.DecryptECB(out, plainLen, key);
-// std::cout << "ENCRYPTED: " << out << endl;
-// std::cout << "PLAIN: " << innew << endl;
-// bool result = nipoLog.write("TEST", 1, nipoLog.type.warn);
