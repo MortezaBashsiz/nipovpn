b1de731339ad710b05c9dab2c1e8042f14dc4ef7
Morteza Bashsiz <morteza.bashsiz@gmail.com>
2024-08-06 12:38:02 +0200

    add some comments

--- src/serverhandler.cpp
+++ src/serverhandler.cpp
@@ -1,5 +1,13 @@
 #include "serverhandler.hpp"
 
+/*
+ * Constructor to initialize the ServerHandler object.
+ * @param readBuffer - Buffer for reading incoming data.
+ * @param writeBuffer - Buffer for writing outgoing data.
+ * @param config - Shared pointer to the configuration object.
+ * @param log - Shared pointer to the logging object.
+ * @param client - Shared pointer to the TCP client object.
+ */
 ServerHandler::ServerHandler(boost::asio::streambuf& readBuffer,
                              boost::asio::streambuf& writeBuffer,
                              const std::shared_ptr<Config>& config,
@@ -12,30 +20,46 @@ ServerHandler::ServerHandler(boost::asio::streambuf& readBuffer,
       writeBuffer_(writeBuffer),
       request_(HTTP::create(config, log, readBuffer)) {}
 
+/*
+ * Destructor for cleanup. No specific resources to release.
+ */
 ServerHandler::~ServerHandler() {}
 
+/*
+ * Handles the incoming request by processing it based on its type (HTTP/HTTPS).
+ * It performs decryption, handles connection, and processes the request.
+ */
 void ServerHandler::handle() {
+  // Detects the type of request (HTTP/HTTPS).
   if (request_->detectType()) {
+    // Log the received request from the agent.
     log_->write(
         "[ServerHandler handle] [Request From Agent] : " + request_->toString(),
         Log::Level::DEBUG);
+
+    // Decrypt the request body using AES256.
     BoolStr decryption{false, std::string("FAILED")};
     decryption = aes256Decrypt(decode64(boost::lexical_cast<std::string>(
                                    request_->parsedHttpRequest().body())),
                                config_->agent().token);
+
     if (decryption.ok) {
+      // Log successful decryption and process the request.
       log_->write(
           "[ServerHandler handle] [Token Valid] : " + request_->toString(),
           Log::Level::DEBUG);
       auto tempHexArr = strToHexArr(decryption.message);
       std::string tempHexArrStr(tempHexArr.begin(), tempHexArr.end());
       copyStringToStreambuf(tempHexArrStr, readBuffer_);
+
+      // Re-check the request type and process accordingly.
       if (request_->detectType()) {
         log_->write(
             "[ServerHandler handle] [Request] : " + request_->toString(),
             Log::Level::DEBUG);
         switch (request_->httpType()) {
           case HTTP::HttpType::connect: {
+            // Handle CONNECT request to establish a connection.
             boost::asio::streambuf tempBuff;
             std::iostream os(&tempBuff);
             client_->doConnect(request_->dstIP(), request_->dstPort());
@@ -51,11 +75,13 @@ void ServerHandler::handle() {
           } break;
           case HTTP::HttpType::http:
           case HTTP::HttpType::https: {
+            // Handle HTTP or HTTPS requests.
             if (!client_->socket().is_open())
               client_->doConnect(request_->dstIP(), request_->dstPort());
             client_->doWrite(readBuffer_);
             client_->doRead();
             if (client_->readBuffer().size() > 0) {
+              // Encrypt the response and send it back.
               BoolStr encryption{false, std::string("FAILED")};
               encryption =
                   aes256Encrypt(streambufToString(client_->readBuffer()),
@@ -67,6 +93,7 @@ void ServerHandler::handle() {
                 if (request_->httpType() == HTTP::HttpType::http)
                   client_->socket().close();
               } else {
+                // Log encryption failure and close the connection.
                 log_->write("[ServerHandler handle] [Encryption Failed] : [ " +
                                 decryption.message + " ] ",
                             Log::Level::DEBUG);
@@ -76,16 +103,19 @@ void ServerHandler::handle() {
                 client_->socket().close();
               }
             } else {
+              // Close the connection if no data was read.
               client_->socket().close();
             }
           } break;
         }
       } else {
+        // Log non-HTTP requests if detected.
         log_->write("[ServerHandler handle] [NOT HTTP Request] [Request] : " +
                         streambufToString(readBuffer_),
                     Log::Level::DEBUG);
       }
     } else {
+      // Log decryption failure and close the connection.
       log_->write("[ServerHandler handle] [Decryption Failed] : [ " +
                       decryption.message + " ] ",
                   Log::Level::DEBUG);
@@ -95,6 +125,7 @@ void ServerHandler::handle() {
       client_->socket().close();
     }
   } else {
+    // Log if the request is not from an agent.
     log_->write(
         "[ServerHandler handle] [NOT HTTP Request From Agent] [Request] : " +
             streambufToString(readBuffer_),
--- src/tcpclient.cpp
+++ src/tcpclient.cpp
@@ -3,6 +3,17 @@
 /*
  * TCPClient
  */
+
+/*
+ * Constructor for the TCPClient class.
+ * Initializes the TCP client with given configuration and logging objects,
+ * and sets up the required I/O context, socket, resolver, and timer.
+ *
+ * @param io_context - The Boost Asio I/O context to be used for asynchronous
+ * operations.
+ * @param config - Shared pointer to the configuration object.
+ * @param log - Shared pointer to the logging object.
+ */
 TCPClient::TCPClient(boost::asio::io_context& io_context,
                      const std::shared_ptr<Config>& config,
                      const std::shared_ptr<Log>& log)
@@ -13,36 +24,62 @@ TCPClient::TCPClient(boost::asio::io_context& io_context,
       resolver_(io_context),
       timer_(io_context) {}
 
+/*
+ * Getter for the socket.
+ *
+ * @return Reference to the Boost Asio TCP socket.
+ */
 boost::asio::ip::tcp::socket& TCPClient::socket() { return socket_; }
 
+/*
+ * Moves the contents of the given stream buffer to the internal write buffer.
+ *
+ * @param buffer - Stream buffer containing data to be written.
+ */
 void TCPClient::writeBuffer(boost::asio::streambuf& buffer) {
   moveStreambuf(buffer, writeBuffer_);
 }
 
+/*
+ * Connects the TCP client to a specified destination IP and port.
+ * Logs the connection attempt and handles any connection errors.
+ *
+ * @param dstIP - Destination IP address as a string.
+ * @param dstPort - Destination port number.
+ */
 void TCPClient::doConnect(const std::string& dstIP,
                           const unsigned short& dstPort) {
   try {
+    // Log connection attempt
     log_->write("[TCPClient doConnect] [DST " + dstIP + ":" +
                     std::to_string(dstPort) + " ]",
                 Log::Level::DEBUG);
-
     log_->write(
         "[Connect to] [DST " + dstIP + ":" + std::to_string(dstPort) + " ]",
         Log::Level::INFO);
 
+    // Resolve the endpoint and connect
     boost::asio::ip::tcp::resolver resolver(io_context_);
     auto endpoint =
         resolver.resolve(dstIP.c_str(), std::to_string(dstPort).c_str());
     boost::asio::connect(socket_, endpoint);
   } catch (std::exception& error) {
+    // Log connection errors
     log_->write(std::string("[TCPClient doConnect] ") + error.what(),
                 Log::Level::ERROR);
   }
 }
 
+/*
+ * Writes data from the internal write buffer to the connected socket.
+ * Logs the write operation and handles any write errors.
+ *
+ * @param buffer - Stream buffer containing data to be written.
+ */
 void TCPClient::doWrite(boost::asio::streambuf& buffer) {
   try {
     moveStreambuf(buffer, writeBuffer_);
+    // Log details of the write operation
     log_->write("[TCPClient doWrite] [DST " +
                     socket_.remote_endpoint().address().to_string() + ":" +
                     std::to_string(socket_.remote_endpoint().port()) + "] " +
@@ -53,6 +90,7 @@ void TCPClient::doWrite(boost::asio::streambuf& buffer) {
                     std::to_string(socket_.remote_endpoint().port()) + "] " +
                     "[Bytes " + std::to_string(writeBuffer_.size()) + "] ",
                 Log::Level::TRACE);
+    // Perform the write operation
     boost::system::error_code error;
     boost::asio::write(socket_, writeBuffer_, error);
     if (error) {
@@ -60,18 +98,27 @@ void TCPClient::doWrite(boost::asio::streambuf& buffer) {
                   Log::Level::ERROR);
     }
   } catch (std::exception& error) {
+    // Log exceptions during the write operation
     log_->write(std::string("[TCPClient doWrite] [catch] ") + error.what(),
                 Log::Level::ERROR);
   }
 }
 
+/*
+ * Reads data from the connected socket into the internal read buffer.
+ * Handles data reading, potential errors, and logs the read operation.
+ * Implements a retry mechanism based on configuration settings.
+ */
 void TCPClient::doRead() {
   try {
+    // Clear the read buffer
     readBuffer_.consume(readBuffer_.size());
     boost::system::error_code error;
+    // Read at least 1 byte from the socket
     boost::asio::read(socket_, readBuffer_, boost::asio::transfer_exactly(1),
                       error);
     if (socket_.available() > 0) {
+      // Implement retry mechanism if data is still available
       boost::asio::deadline_timer timer{io_context_};
       for (auto i = 0; i <= config_->general().repeatWait; i++) {
         while (true) {
@@ -95,12 +142,14 @@ void TCPClient::doRead() {
     }
     if (readBuffer_.size() > 0) {
       try {
+        // Log the successful read operation
         log_->write("[TCPClient doRead] [SRC " +
                         socket_.remote_endpoint().address().to_string() + ":" +
                         std::to_string(socket_.remote_endpoint().port()) +
                         "] [Bytes " + std::to_string(readBuffer_.size()) + "] ",
                     Log::Level::DEBUG);
       } catch (std::exception& error) {
+        // Log exceptions during logging
         log_->write(
             std::string("[TCPClient doRead] [catch log] ") + error.what(),
             Log::Level::ERROR);
@@ -109,6 +158,7 @@ void TCPClient::doRead() {
       socket_.close();
     }
   } catch (std::exception& error) {
+    // Log exceptions during the read operation
     log_->write(std::string("[TCPClient doRead] [catch read] ") + error.what(),
                 Log::Level::ERROR);
     return;
