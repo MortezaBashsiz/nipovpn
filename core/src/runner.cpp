5b0a7480f50a91fb7a73d64916d9fe11f278789b
Morteza Bashsiz <morteza.bashsiz@gmail.com>
2024-09-26 10:07:01 +0200

    Hot fix, segmentation fault on runner

--- core/src/runner.cpp
+++ core/src/runner.cpp
@@ -1,17 +1,24 @@
 #include "runner.hpp"
 
-
 Runner::Runner(const std::shared_ptr<Config> &config,
                const std::shared_ptr<Log> &log)
     : config_(config),
       log_(log),
       io_context_(),
-      work_guard_(boost::asio::make_work_guard(io_context_)),
+      work_guard_(boost::asio::make_work_guard(
+              io_context_)),
       running_(true),
       strand_(boost::asio::make_strand(io_context_)) {}
 
 Runner::~Runner() {
-    stop();
+    running_.store(
+            false);
+    io_context_.stop();
+    for (auto &thread: threadPool_) {
+        if (thread.joinable()) {
+            thread.join();
+        }
+    }
 }
 
 void Runner::run() {
@@ -21,13 +28,19 @@ void Runner::run() {
                     Log::Level::INFO);
         log_->write(config_->toString(), Log::Level::INFO);
 
+
         auto tcpServer = TCPServer::create(io_context_, config_, log_);
 
-        tcpServer->startAccept();
 
-        unsigned int numThreads = config_->threads() > 0 ? config_->threads() : std::thread::hardware_concurrency();
-        for (unsigned int i = 0; i < numThreads; ++i) {
+        for (auto i = 0; i < config_->threads(); ++i) {
             threadPool_.emplace_back([this] { workerThread(); });
+        }
+
+
+        for (auto &thread: threadPool_) {
+            if (thread.joinable()) {
+                thread.join();
+            }
         }
     } catch (const std::exception &error) {
 
@@ -42,9 +55,10 @@ void Runner::workerThread() {
     while (running_.load()) {
         try {
             io_context_.run();
-            io_context_.restart();
+            break;
         } catch (const std::exception &e) {
-            log_->write(std::string("[WorkerThread] Exception: ") + e.what(), Log::Level::ERROR);
+            log_->write(std::string("[WorkerThread] Exception: ") + e.what(),
+                        Log::Level::ERROR);
         } catch (...) {
             log_->write("[WorkerThread] Unknown error occurred", Log::Level::ERROR);
         }
@@ -54,9 +68,4 @@ void Runner::workerThread() {
 void Runner::stop() {
     running_.store(false);
     io_context_.stop();
-    for (auto &thread: threadPool_) {
-        if (thread.joinable()) {
-            thread.join();
-        }
-    }
 }
